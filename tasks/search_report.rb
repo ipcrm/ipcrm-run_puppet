aea18e425de7fb38b2b37a5ed7435c86e30bbd65
ipcrm <mcadorette@gmail.com>
2017-12-03 12:33:47 -0500

    fix search task

--- tasks/search_report.rb
+++ tasks/search_report.rb
@@ -10,15 +10,11 @@ role = params['role']
 resource = params['resource']
 
 query = 'reports[certname,logs]{ latest_report? = true and facts { name = "role" and value = "%s" } }' % [role]
-cmd_string = "/opt/puppetlabs/bin/puppet-query #{query}"
-_,stdout,_,_ = Open3.popen3(cmd_string)
+cmd_string = "/opt/puppetlabs/bin/puppet-query"
+_,stdout,_,_ = Open3.popen3(cmd_string,query)
 
-
-out = stdout.readlines
-out.pop
-out.shift
-
-
-JSON.parse(out)["logs"]["data"].each do |l|
-  printf "%-30s %s\n" % [ c["certname"].rstrip(), l["message"].split(/[\[,\]]/)[1] ] if l["message"].include?("#{resource}")
+JSON.parse(stdout.read).each do |c|
+  c["logs"]["data"].each do |l|
+    printf "%-30s %s\n" % [ c["certname"].rstrip(), l["message"].split(/[\[,\]]/)[1] ] if l["message"].include?("#{resource}")
+  end
 end
