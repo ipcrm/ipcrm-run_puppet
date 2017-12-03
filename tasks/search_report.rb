fbb8285b8d893c16fc8a92b9a05bbe3f4bbb8971
ipcrm <mcadorette@gmail.com>
2017-12-03 11:28:57 -0500

    init search_report

---
+++ tasks/search_report.rb
@@ -0,0 +1,20 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+
+require 'puppet'
+require 'json'
+require 'open3'
+
+_param = STDIN.read
+role = params['role']
+resource = params['resource']
+
+query = 'reports[certname,logs]{ latest_report? = true and facts { name = "role" and value = "%s" } }' % [role]
+cmd_string = "/opt/puppetlabs/bin/puppet-query #{query}"
+_,stdout,_,_ = Open3.popen3(cmd_string)
+
+
+JSON.parse(stdout.readlines).each do |c|
+  c["logs"]["data"].each do |l|
+    printf "%-30s %s\n" % [ c["certname"].rstrip(), l["message"].split(/[\[,\]]/)[1] ] if l["message"].include?("#{resource}")
+  end
+end
