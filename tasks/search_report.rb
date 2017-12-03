59f71b473a31507cc450194dfcdb3637a23fc308
ipcrm <mcadorette@gmail.com>
2017-12-03 11:39:43 -0500

    fix parsing of readlines

--- tasks/search_report.rb
+++ tasks/search_report.rb
@@ -5,7 +5,7 @@ require 'json'
 require 'open3'
 
 _param = STDIN.read
-params = JSON.parse(_param)
+params = JSON.parse(_param)[0]
 role = params['role']
 resource = params['resource']
 
@@ -14,7 +14,7 @@ cmd_string = "/opt/puppetlabs/bin/puppet-query #{query}"
 _,stdout,_,_ = Open3.popen3(cmd_string)
 
 
-JSON.parse(stdout.readlines).each do |c|
+JSON.parse(stdout.readlines[0]).each do |c|
   c["logs"]["data"].each do |l|
     printf "%-30s %s\n" % [ c["certname"].rstrip(), l["message"].split(/[\[,\]]/)[1] ] if l["message"].include?("#{resource}")
   end
