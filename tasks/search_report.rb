9bc264e52a8b64019dd08eb818298f80d2510bfe
ipcrm <mcadorette@gmail.com>
2017-12-03 11:48:23 -0500

    fix iteration logic

--- tasks/search_report.rb
+++ tasks/search_report.rb
@@ -14,8 +14,11 @@ cmd_string = "/opt/puppetlabs/bin/puppet-query #{query}"
 _,stdout,_,_ = Open3.popen3(cmd_string)
 
 
-JSON.parse(stdout.readlines[0]).each do |c|
-  c["logs"]["data"].each do |l|
-    printf "%-30s %s\n" % [ c["certname"].rstrip(), l["message"].split(/[\[,\]]/)[1] ] if l["message"].include?("#{resource}")
-  end
+out = stdout.readlines
+out.pop
+out.shift
+
+
+JSON.parse(out)["logs"]["data"].each do |l|
+  printf "%-30s %s\n" % [ c["certname"].rstrip(), l["message"].split(/[\[,\]]/)[1] ] if l["message"].include?("#{resource}")
 end
